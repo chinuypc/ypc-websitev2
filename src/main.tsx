8131077622f231c12fbcedd62e4a144375acb01c
minaypclub <mina@ypclub.com>
2026-03-12 19:27:23 +0200

    add mispanel, google tags, linkedin, sitemap

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
+  import mixpanel from "mixpanel-browser";
+
+  mixpanel.init("36ebcf022dfaf8914069d6a730a60c50", { debug: true, autocapture: true, record_sessions_percent: 100 });
 
   createRoot(document.getElementById("root")!).render(<App />);
   
